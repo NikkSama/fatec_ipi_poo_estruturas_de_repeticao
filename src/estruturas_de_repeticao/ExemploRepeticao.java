8e06fd09b5d7eb1a8b56319190b1ae8f152c36dc
NikkSama <audrey.sampaiobrito@gmail.com>
2021-08-19 16:15:27 -0300

    Terminamos o exemplo com for

--- src/estruturas_de_repeticao/ExemploRepeticao.java
+++ src/estruturas_de_repeticao/ExemploRepeticao.java
@@ -6,19 +6,32 @@ public class ExemploRepeticao {
     public static void main(String[] args) {
         Random gerador = new Random();
         double nota1, nota2;
-        //repeticao controlada por contador
-        int totalAlunos = 1;
-        while (totalAlunos <= 5) {
+
+        //inicialização (incluindo,talvez, a decalaracao
+        //teste de continuidade
+        //incremento
+        for (int totalAlunos = 1; totalAlunos <= 5; totalAlunos++){
             nota1 = gerador.nextDouble() * 10;
             nota2 = gerador.nextDouble() * 10;
-            double resultado = (nota1 + nota2) / 2;
+            System.out.println(
+                    String.format("Média: %.2f", (nota1+nota2)/2));
 
-            System.out.printf("Média: %.2f\n", resultado);
-            totalAlunos ++;
-            //++totalAlunos;
-            //totalAlunos += 1;
-            //totalAlunos = totalAlunos + 1;
         }
-        System.out.println("Até mais.");
+
+
+//        //repeticao controlada por contador
+//        int totalAlunos = 1;
+//        while (totalAlunos <= 5) {
+//            nota1 = gerador.nextDouble() * 10;
+//            nota2 = gerador.nextDouble() * 10;
+//            double resultado = (nota1 + nota2) / 2;
+//
+//            System.out.printf("Média: %.2f\n", resultado);
+//            totalAlunos ++;
+//            //++totalAlunos;
+//            //totalAlunos += 1;
+//            //totalAlunos = totalAlunos + 1;
+//        }
+//        System.out.println("Até mais.");
     }
 }
